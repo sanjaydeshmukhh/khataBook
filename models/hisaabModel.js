08d86652cc99d135574c183cc42e7c66667fd9fc
Sanjay Deshmukh <sd876400@gmail.com>
2024-06-22 12:31:34 +0530

    hisab model update

--- models/hisaabModel.js
+++ models/hisaabModel.js
@@ -1,13 +1,37 @@
 const mongoose = require("mongoose")
 
 const hisaabModel = mongoose.Schema({
-    title: String,
-    description: String, 
-    user: mongoose.Schema.Types.ObjectId,
-    editable: Boolean,
-    shareable: Boolean,
-    encrypted: Boolean,
-    passcode: Number
-})
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true, // Removes whitespace from both ends of the string
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+        trim: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "User ID is required"],
+        ref: "User", // Reference to the User model
+    },
+    editable: {
+        type: Boolean,
+        default: true,
+    },
+    shareable: {
+        type: Boolean,
+        default: false,
+    },
+    encrypted: {
+        type: Boolean,
+        default: false,
+    },
+    passcode: {
+        type: Number,
+        required: [true, "Passcode is required"],
+    },
+});
 
 module.exports = mongoose.model("hisaab", hisaabModel)
