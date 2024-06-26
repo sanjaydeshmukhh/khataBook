c9bd6088f1e5bbaeb92a04180cd1c0abb2001a68
Sanjay Deshmukh <sd876400@gmail.com>
2024-06-26 16:14:51 +0530

    register route

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,35 @@
-const express = require("express")
+// const express = require("express")
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-module.exports.indexController = function(req, res){
-    res.render("index")
+module.exports.indexController = function (req, res) {
+    res.render("index");
+};
+
+module.exports.registerController = function (req, res) {
+    res.render("register");
+};
+module.exports.postRegisterController = async function (req, res) {
+    let { email, username, name, password } = req.body;
+    try{
+    let user = await userModel.findOne({ email });
+    if (user) return res.render("you already have an account, please login");
+
+    let salt = await bcrypt.genSalt(10);
+    let hashed = await bcrypt.hash(password, salt);
+
+    user = await userModel.create({ email, 
+        name, 
+        username, 
+        password: hashed
+     });
+
+     let jwt = jwt.sign({id: user._id, email: user._email }, process.env.JWT_KEY)
+
+     res.cookie("token", token)
+     res.send("account created succesfully")
 }
-
-module.exports.registerController = function(req, res){
-    res.render("register")
+catch (err){
+    res.send(err.message)
 }
-
+};
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {indexController, registerController}  = require("../controllers/indexController");
+const {indexController, registerController, postRegisterController}  = require("../controllers/indexController");
 
 router.get("/", indexController)
 router.get("/register", registerController)
+router.post("/register", postRegisterController)
+
 
 module.exports = router;
